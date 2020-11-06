4b74e056ab8cfde3fe46ab935a5211c3431c02d1
leejeongseok <jsuk10@naver.com>
2020-11-06 18:30:53 +0900

    feat : If reflected when refreshed

--- js/main.js
+++ js/main.js
@@ -50,12 +50,26 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum* window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
         }
+
+        let totalScrollHeight = 0;
+
+        yOffset = window.pageYOffset;
+        for(let i = 0 ; i < sceneInfo.length ; i++){
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
+        
     }
 
 
+    //스크롤시 현재 스크롤 값에 따라 섹션을 정해주고 보이는 아이디를 바꿔줌
     function scrollLoop(){
         prevScrollHeight = 0;
-        console.log(currentScene);
 
         for(let i = 0 ; i<currentScene;i++){
             prevScrollHeight += sceneInfo[i].scrollHeight;
@@ -73,13 +87,27 @@
             }
         }
 
+        // let totalScrollHeight = 0;
+
+        // for(let i = 0 ; i < sceneInfo.length ; i++){
+        //     totalScrollHeight += sceneInfo[i].scrollHeight;
+        //     if (totalScrollHeight >= yOffset) {
+        //         currentScene = i;
+        //         break;
+        //     }
+        // }
+
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
+        
     }
 
     window.addEventListener('resize', setLayout);
+    window.addEventListener('load', setLayout);
+
     window.addEventListener('scroll', () =>{
         yOffset = window.pageYOffset;
         scrollLoop();
-});
+    });
 
     setLayout();
 })();
