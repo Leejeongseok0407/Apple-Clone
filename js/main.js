8a480937cecbf32c1f4a4f2120fffb3b6a0063c5
leejeongseok <jsuk10@naver.com>
2020-11-06 17:33:16 +0900

    feat: Decide scene response to scrolling
    스크롤값을 통하여 현재 어떤 씬을 보고있는지 선택하게 해줌
    
    key: window.pageYOffset, window.innerHeight

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (() => {
+    
+    let yOffset = 0; //window.pageYOffset 대신 사용
+    let prevScrollHeight = 0; //현재 스크롤 보다 높은 값의 섹션들의 높이 합
+    let currentScene = 0; //현재 씬
+
     const sceneInfo =[
         {
             //0
@@ -38,6 +43,7 @@
         }
     ];
     
+
     // 각 스크롤 섹션의 높이를 세팅함
     function setLayout(){
         for(let i = 0; i < sceneInfo.length ; i++){
@@ -46,7 +52,34 @@
         }
     }
 
+
+    function scrollLoop(){
+        prevScrollHeight = 0;
+        console.log(currentScene);
+
+        for(let i = 0 ; i<currentScene;i++){
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+        
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+            if(currentScene !== 3){
+                currentScene++;
+            }
+        }
+        
+        if(yOffset < prevScrollHeight){
+            if(currentScene !==0){
+                currentScene--;
+            }
+        }
+
+    }
+
     window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () =>{
+        yOffset = window.pageYOffset;
+        scrollLoop();
+});
 
     setLayout();
 })();
