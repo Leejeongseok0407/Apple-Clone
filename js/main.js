a00d17766d83a1b3d14b1d96b7e4346fdbf9f23d
jsuk10 <jsuk10@naver.com>
2020-12-12 17:51:02 +0900

    feat : caption oppacity & translate setting

--- js/main.js
+++ js/main.js
@@ -105,8 +105,10 @@
         //출발값, 최종값으로 구성됨.
         rect1X: [0, 0, { start: 0, end: 0 }],
         rect2X: [0, 0, { start: 0, end: 0 }],
-        blendHeight:[0, 0, { start: 0, end: 0 }],
-        canvas_scale:[0, 0, { start: 0, end: 0 }],
+        blendHeight: [0, 0, { start: 0, end: 0 }],
+        canvas_scale: [0, 0, { start: 0, end: 0 }],
+        canvasCaption_oppacity: [0, 1, { start: 0, end: 0 }],
+        canvasCaption_translateY: [10, 0, { start: 0, end: 0 }],
         rectStartY: 0,
       },
     },
@@ -285,49 +287,39 @@
           objs.pinC.style.transform = `scaleY(${calcValues(values.pinC_scaleY, currentYOffset)})`;
         }
 
-        
-
         //3번 캔버스 그리기
-          if(scrollRatio>0.9){
-            const objs = sceneInfo[3].objs;
-            const values = sceneInfo[3].values;
-            const wdithRatio = window.innerWidth / objs.canvas.width;
-            const heightRatio = window.innerHeight / objs.canvas.height;
-            let canvasScaleRatio;
-    
-            if (wdithRatio <= heightRatio) {
-              canvasScaleRatio = heightRatio;
-            } else {
-              canvasScaleRatio = wdithRatio;
-            }
-            
-            const recalculatedInnerWidth = document.body.offsetWidth / canvasScaleRatio;
-            const whiteRectWidth = recalculatedInnerWidth * 0.15;
+        if (scrollRatio > 0.9) {
+          const objs = sceneInfo[3].objs;
+          const values = sceneInfo[3].values;
+          const wdithRatio = window.innerWidth / objs.canvas.width;
+          const heightRatio = window.innerHeight / objs.canvas.height;
+          let canvasScaleRatio;
 
-            objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
-            objs.context.drawImage(objs.images[0], 0, 0);
-            objs.context.fillStyle = 'white';
-    
-            values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
-            values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
-            values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
-            values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
-    
-            objs.context.fillRect(
-              parseInt(values.rect1X[0]),
-              0,
-              parseInt(whiteRectWidth),
-              objs.canvas.height);
-    
-            objs.context.fillRect(
-              parseInt(values.rect2X[0]),
-              0,
-              parseInt(whiteRectWidth),
-              objs.canvas.height);
+          if (wdithRatio <= heightRatio) {
+            canvasScaleRatio = heightRatio;
+          } else {
+            canvasScaleRatio = wdithRatio;
           }
 
+          const recalculatedInnerWidth = document.body.offsetWidth / canvasScaleRatio;
+          const whiteRectWidth = recalculatedInnerWidth * 0.15;
+
+          objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+          objs.context.drawImage(objs.images[0], 0, 0);
+          objs.context.fillStyle = "white";
+
+          values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+          values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+          values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+          values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+
+          objs.context.fillRect(parseInt(values.rect1X[0]), 0, parseInt(whiteRectWidth), objs.canvas.height);
+
+          objs.context.fillRect(parseInt(values.rect2X[0]), 0, parseInt(whiteRectWidth), objs.canvas.height);
+        }
+
         break;
-    
+
       //캔버스에 이미지를 넣고 양쪽에 블럭을 채워서 이미지가 커져 보이는 효과를 줌.
       case 3:
         let step = 0;
@@ -335,7 +327,7 @@
         const wdithRatio = window.innerWidth / objs.canvas.width;
         const heightRatio = window.innerHeight / objs.canvas.height;
         let canvasScaleRatio;
-        
+
         if (wdithRatio <= heightRatio) {
           canvasScaleRatio = heightRatio;
         } else {
@@ -347,9 +339,9 @@
 
         objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
         objs.context.drawImage(objs.images[0], 0, 0);
-        objs.context.fillStyle = 'white';
+        objs.context.fillStyle = "white";
 
-        if(values.rectStartY === 0){
+        if (values.rectStartY === 0) {
           values.rectStartY = objs.canvas.offsetTop + canvasHeightSizeDifference;
 
           values.rect1X[2].start = window.innerHeight / 2 / scrollHeight;
@@ -357,54 +349,54 @@
           values.rect1X[2].end = values.rectStartY / scrollHeight;
           values.rect2X[2].end = values.rectStartY / scrollHeight;
         }
-        
 
         values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
         values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
         values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
         values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
 
-        objs.context.fillRect(
-          parseInt(calcValues(values.rect1X,currentYOffset)),
-          0,
-          parseInt(whiteRectWidth),
-          objs.canvas.height);
+        objs.context.fillRect(parseInt(calcValues(values.rect1X, currentYOffset)), 0, parseInt(whiteRectWidth), objs.canvas.height);
 
-        objs.context.fillRect(
-          parseInt(calcValues(values.rect2X,currentYOffset)),
-          0,
-          parseInt(whiteRectWidth),
-          objs.canvas.height);
+        objs.context.fillRect(parseInt(calcValues(values.rect2X, currentYOffset)), 0, parseInt(whiteRectWidth), objs.canvas.height);
 
-        if(scrollRatio < values.rect1X[2].end){
+        if (scrollRatio < values.rect1X[2].end) {
           step = 1;
           objs.canvas.classList.remove(`sticky`);
-        }else{
+        } else {
           step = 2;
           values.blendHeight[0] = 0;
           values.blendHeight[1] = objs.canvas.height;
           values.blendHeight[2].start = values.rect1X[2].end;
           values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
-          const blendHeight = calcValues(values.blendHeight,currentYOffset);
+          const blendHeight = calcValues(values.blendHeight, currentYOffset);
 
-          objs.context.drawImage(objs.images[1]
-            ,0,objs.canvas.height - blendHeight,objs.canvas.width,blendHeight
-            ,0,objs.canvas.height - blendHeight,objs.canvas.width,blendHeight
-            );
-          
+          objs.context.drawImage(objs.images[1], 0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight, 0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight);
+
           objs.canvas.classList.add(`sticky`);
-          objs.canvas.style.top=`${-canvasHeightSizeDifference}px`;
+          objs.canvas.style.top = `${-canvasHeightSizeDifference}px`;
 
-          if(scrollRatio>values.blendHeight[2].end){
+          if (scrollRatio > values.blendHeight[2].end) {
             values.canvas_scale[0] = canvasScaleRatio;
-            values.canvas_scale[1] = document.body.offsetWidth / (objs.canvas.width *1.5);
+            values.canvas_scale[1] = document.body.offsetWidth / (objs.canvas.width * 1.5);
             values.canvas_scale[2].start = values.blendHeight[2].end;
             values.canvas_scale[2].end = values.canvas_scale[2].start + 0.2;
-            console.log(values.canvas_scale);
-            objs.canvas.style.transform = `scale(${calcValues(values.canvas_scale,currentYOffset)})`;
+            objs.canvas.style.transform = `scale(${calcValues(values.canvas_scale, currentYOffset)})`;
+            objs.canvas.style.marginTop = `${0}px`;
+            objs.canvasCaption.style.opacity = 0;
+          }
+
+          if (scrollRatio > values.canvas_scale[2].end && values.canvas_scale[2].end > 0) {
+            console.log("dd");
+            objs.canvas.classList.remove(`sticky`);
+            objs.canvas.style.marginTop = `${scrollHeight * 0.4}px`;
+            values.canvasCaption_oppacity[2].start = values.canvas_scale[2].end;
+            values.canvasCaption_oppacity[2].end = values.canvasCaption_oppacity[2].start + 0.1;
+            values.canvasCaption_translateY[2].start = values.canvasCaption_oppacity[2].start;
+            values.canvasCaption_translateY[2].end = values.canvasCaption_oppacity[2].end;
+            objs.canvasCaption.style.opacity = calcValues(values.canvasCaption_oppacity, currentYOffset);
+            objs.canvasCaption.style.transform = `translate3d(0,${calcValues(values.canvasCaption_translateY, currentYOffset)}%,0)`;
           }
         }
-
         break;
     }
   }
